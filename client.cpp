b09f7a88369ab0eb5bf2f08d3f8aa41d160f1431
Ryan Khamneian <ryan2kool@yahoo.com>
2022-01-24 12:47:56 -0800

    code before testing

---
+++ client.cpp
@@ -0,0 +1,253 @@
+/*
+ * This file will create a new socket, connect to the server, and send data uisng
+ * three different ways of writing data/ transferring data. Once it does this,
+ * it will wait for a response and print it.
+ */
+
+// header files provided by professor. Needed to call the OS functions
+// windows doesnt have most of these headers provided so need to run this on a linux system
+#include <sys/types.h>    // socket, bind 
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa 
+#include <netinet/in.h>   // htonl, htons, inet_ntoa 
+#include <arpa/inet.h>    // inet_ntoa 
+#include <netdb.h>        // gethostbyname 
+#include <unistd.h>       // read, write, close 
+#include <strings.h>      // bzero 
+#include <netinet/tcp.h>  // SO_REUSEADDR 
+#include <sys/uio.h>      // writev
+
+#include <iostream>
+#include <stdio.h>
+#include <cstring> // for memset
+#include <sys/time.h> // needed for gettimeofday()
+
+using namespace std;
+
+/*
+ * This function returns the head of a linked list of guesses for the socket information.
+ * Uses the getaddrinfo() function provided by socket.h to acquire this.
+ * Needed because connect() needs specific information about the socket to connect.
+ * We feed this function the port number of the process runningo on the server, the ip address
+ * of the server, and a set of hints for locating this socket information.
+ * getaddrinfo itself returns an integer representing if it worked or not so we feed it the resulting
+ * linked list of addrinfo structures.
+ */
+struct addrinfo* getAddressGuesses (char* portNumber, char* ipAddress) {
+    // give the following hints of the server
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // tells the hint that the address family is internet
+	hints.ai_socktype = SOCK_STREAM; // tells hint that it is a stream socket protocol
+	hints.ai_protocol = IPPROTO_TCP; // TCP isnt the only sock stream protocol so we need to explicitly say TCP
+
+    // the resulting pointer to the first address of the addrinfo linked list (the first guess which points to the next ones)
+    struct addrinfo* results;
+
+    // when calling the getaddrinfo() function, it takes hints as a pointer to a addrinfo struct
+    // and results as a double pointer. So we need to dereference them with the ampersand
+
+    int status = getaddrinfo(ipAddress, portNumber, &hints, &results);
+
+    // if status is not 0, it means that the function failed, so printing an error
+    if (status != 0) {
+        cout << "Could not connect to socket with provided port number and ip address" << endl;
+        exit (EXIT_FAILURE); // exits the program
+    }
+
+    // otherwise, the results variable holds a pointer to the first guess of socket information in the list, so can return it
+    return results;
+}
+
+/*
+ * Returns a socket descriptor to the process running on the server side
+ * Loops through the linked list of guesses of socket information and tries to connect to each.
+ * If one of them works, returns its descriptor. Head is just the guesses pointer as its the first guess.
+ */
+int getSocketDescriptor (struct addrinfo* head) {
+    struct addrinfo* curr = head;
+
+    int sd = -1; // the return value
+
+    while (curr != NULL) {
+        // Step 1 - Create the socket to test with the information from the guess (3 parameters)
+        // Parameter 1 - Domain (an integer representing the family (ipv4/ ipv6))
+        // Parameter 2 - Type (an integer representing the type (datagram vs. stream))
+        // Parameter 3 - Protocol (an integer for the protocol (TCP vs. UDP))
+
+        // socket descriptor points to a index in a table (like a file descriptor)
+        // this call is creating that socket and returning the index of that socket in the table
+        // this client.cpp program is going to be using this socket
+        sd = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
+
+        // if sd is -1, it means it cant connect so try next guess
+        if (sd == -1) {
+            curr = curr->ai_next;
+        } // step 2 - check to see if you can actually connect
+        else {
+            // if it didnt return -1, it returned 1 meaning success, so check to see if it can actually connect
+            // to do that, call connect with three parameters
+            // 1. The socket descriptor we just recieved
+            // 2. The address information of the socket
+            // 3. The length of the address
+            int successfulConnection = connect(sd, curr->ai_addr, curr->ai_addrlen);
+
+            // if this is not -1, it means it failed and we should move on
+            if (successfulConnection != -1) {
+                curr = curr->ai_next;
+            }
+            else {
+                close(sd);
+            }
+        }
+    }
+
+    return sd;
+}
+
+/*
+ * This function creates a data buffer and uses the write() and writev() sys calls to send write
+ * information over to the process running on the server. The type variable describes the type of
+ * write we are doing (described over each if statement)
+ */
+void writeToSocket (int iterations, int nbufs, int bufsize, int type, int socketDescriptor) {
+    // allocate a data buffer to send to the server
+	// data buffers are temporary storage to transfer between different media and storage
+	// basically a array of strings (array of array of characters) with each string sized (bufsize - in bytes)
+	// and the number of strings is equivalent to the nbufs (number of data buffers)
+	char databuf[nbufs][bufsize]; // where nbufs * bufsize = 1500 (whole load is 1500 bytes)
+
+    // incase it is type 2, need to break the data to segments
+    struct iovec vector[nbufs];
+
+	// the assignment says to call the write iteration times in all cases so does this in a loop iteration times
+	for (int i = 0; i < iterations; i++) {
+		if(type == 1) {
+			// if type is 1, we do multiple writes so send each string individually
+			for(int j = 0; j < nbufs; j++){
+				write(socketDescriptor, databuf[j], bufsize); // each buffer is size buffsize
+			}
+		}
+		else if(type == 2){
+			// single write all the buffers
+			int length = nbufs * bufsize;
+			write(socketDescriptor, databuf, length);
+		}
+		else{
+            // store the all the segments with the base and length
+			for (int j = 0; j < nbufs; j++) {
+                vector[j].iov_base = databuf[j];
+                vector[j].iov_len = bufsize;
+            }
+
+            // write over the segments
+            write (socketDescriptor, vector, bufsize);
+		}
+	}
+}
+
+int main(int argc, char** argv) {
+    char* serverPort = argv[1]; // first argument is the port to the server
+	char* serverName = argv[2]; // servers IP address or host name
+	int iterations = atoi(argv[3]); // number of iterations a client performs on data transmission using one of the three methods
+	int nbufs = atoi(argv[4]); // the number of data buffers
+	int bufsize = atoi(argv[5]); // the size of each data buffer (in bytes)
+	int type = atoi(argv[6]); // the type of transfer scenario (1 for "multiple writes", 2 for "writev", 3 for "single write")
+
+	// if the type is not between 1 and 3, need to tell user as it is not valid
+	if(type < 1 || type > 3){
+		cout << "type must be bteween 1 and 3" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// get the linked list of addrinfo that the connection() can understand
+	struct addrinfo* addressGuesses = getAddressGuesses(serverPort, serverName);
+
+    int sd = getSocketDescriptor(addressGuesses);
+
+    // if the socket descriptor is -1, we couldnt find a successful connection so just kill the program
+    if (sd == -1) {
+        cout << "Could not find a successful connection" << endl;
+        exit (EXIT_FAILURE);
+    }
+
+    // need to calculate the transfer time so steps for that are
+
+    // 1. declare needed variables (timeval is just a structure with 2 variables: seconds, and microseconds)
+    struct timeval start; // start time of sending data
+    struct timeval end; // end total time after sending and reading data
+    struct timeval lap; // end time of sending data
+    long transferTime; // lap - start
+    long totalTime; // end - start
+
+    // 2. start the starttime with the current time of day (i believe this is just linux time)
+    gettimeofday(&start, NULL); // sets start tot he current time of day. Second parameter is timezone
+
+
+    // 3. Call the function to write to socket
+    // if we have found a successful connection, write the data to the socket
+    writeToSocket(iterations, nbufs, bufsize, type, sd);
+
+    // 4. When it is done writing, set the value of lap to the current time of day
+    gettimeofday(&start, NULL);
+
+    // 5. Now read back the information from the server (takes some time)
+    int numReads = 0;
+    read(sd, &numReads, sizeof(numReads)); // read takes in a pointer so need to dereference
+
+    // 6. Now check the time after reading (store as end)
+    gettimeofday(&end, NULL);
+
+    // 7. Calculate the transfer time (lap - start) & the total time (end - start)
+    transferTime = ((lap.tv_sec - start.tv_sec) * 1000000L) + (lap.tv_usec - start.tv_usec);
+    totalTime = ((end.tv_sec - start.tv_sec) * 1000000L) + (end.tv_usec - start.tv_usec);
+
+    // 8. Print the values in the format from the spec
+    cout << "data-transmission time = " << transferTime << " usec, round-trip time = "
+        << totalTime << " usec, #reads = " << numReads << endl;
+
+    // 9. Finally, close the socket
+    close(sd);
+
+    return 0;
+}
+
+
+
+
+/*
+ * Notes:
+ * structure for the addrinfo (from the netdb class)
+ *{
+  *int ai_flags;			 Input flags.  
+  *int ai_family;		 Protocol family for socket.  
+  int ai_socktype;		 Socket type.  
+  int ai_protocol;		 Protocol for socket.  
+  socklen_t ai_addrlen;		 Length of socket address.  
+  struct sockaddr *ai_addr;	 Socket address for socket.  
+  char *ai_canonname;		 Canonical name for service location.  
+  struct addrinfo *ai_next;	 Pointer to next in list.  
+};
+
+ Structure describing a generic socket address. From socket.h
+struct sockaddr
+  {
+    __SOCKADDR_COMMON (sa_);	 Common data: address family and length.  
+    char sa_data[14];		 Address data.  
+  };
+
+To write to a socket
+ a) You can use the write system call with a socket just like how you can with a file and file descriptor
+ b) Not sure how this works (TODO: look this up)
+ c) The socket descriptor is just like a file descriptor
+
+Buffer
+ a) A way of transporting temporary information between two storages
+ b) The server needs to handle how that write is dealt with
+
+Socket
+ Create a new socket of type TYPE in domain DOMAIN, using
+   protocol PROTOCOL.  If PROTOCOL is zero, one is chosen automatically.
+   Returns a file descriptor for the new socket, or -1 for errors.  
+extern int socket (int __domain, int __type, int __protocol) __THROW;
+
+ */
---
+++ server.cpp
@@ -0,0 +1,202 @@
+/*
+ * Server File Description:
+ * The server will create a TCP socket that listens on a port. The Port will be the last 4
+ * digits of my student id: 2648. The server will accept an incoming connection and then
+ * create a new thread (using the pthreads library) that will handle the connection. The
+ * new thread will read all the data from the client and respond back to it. This is called
+ * the acknowledgment. In this case, I will send back the number of read() calls made.
+ */
+#include <sys/types.h>    // socket, bind 
+#include <sys/socket.h>   // socket, bind, listen, inet_ntoa 
+#include <netinet/in.h>   // htonl, htons, inet_ntoa 
+#include <arpa/inet.h>    // inet_ntoa 
+#include <netdb.h>        // gethostbyname 
+#include <unistd.h>       // read, write, close 
+#include <strings.h>      // bzero 
+#include <netinet/tcp.h>  // SO_REUSEADDR 
+#include <sys/uio.h>      // writev
+
+#include <iostream>
+#include <stdio.h>
+#include <cstring>
+#include <sys/time.h> // needed for gettimeofday()
+
+using namespace std; // to use cout and endl
+
+struct communicationThreadData {
+	int socketDescriptor;
+	int numIterations;
+};
+
+/*
+ * This function is the meat and potatoes of the server program. Once a connection is accepted and a new thread is
+ * created to handl the communication and reponse, this function is called. It takes in the thread data created
+ * in the main function which has two variables (iterations and the socket descriptor). This function returns to
+ * the client the number of times the buffer was read, and prints to the console the amount of time it took in order
+ * to read all of the data.
+ */
+void* genResponse(void* input) {
+	int comThread = ((struct communicationThreadData*)input)->socketDescriptor; // socket descriptor of the current communication thread
+	int iterations = ((struct communicationThreadData*)input)->numIterations; // number of iterations
+	struct timeval start; // time we start reading
+	struct timeval end; // time we stop reading
+
+	int count; // number of reads
+	int dataRecievingTime; // difference between end time and start time (how long it took to read the data)
+	int BUFSIZE = 1500; // number of bytes in the data buffer (assignment spec says to use 1500)
+
+	char databuf[BUFSIZE]; // data buffer we are reading into
+
+	gettimeofday(&start, NULL); // setting the start time to the current time of day with no input for time zone
+
+	// code from spec sheet
+	// reads in data from the client into the buffer iteration times (same iteration number as client side)
+	for (int i = 0; i < iterations; i++) {
+		for (int nRead = 0; nRead += read(comThread, databuf, BUFSIZE - nRead) < BUFSIZE; count++);
+	}
+
+	// once we are done reading, store the new time of day in the end time
+	gettimeofday(&end, NULL);
+
+	// send acknowledgement (response) back to client
+	// this is the value kept in count (the number of reads we performed on the buffer)
+	// this is done via the write system call which takes in a file descriptor, the data, and the size of the data
+	// in this case, the file descriptor points to a communication link (the socket) which in linux is a file (everything is a file)
+	write(comThread, &count, sizeof(count));
+
+	// print out the time it took to read the data to the console
+	dataRecievingTime = (((end.tv_sec - start.tv_sec) * 1000000L) + (end.tv_usec - start.tv_usec));
+	cout << "data-receiving time = " << dataRecievingTime << " usec" << endl;
+
+	// finally, once the reponse is formulated and sent, and we no longer need to the communication link, close the socket
+	// thus, terminating the file representing the socket and opening up that descriptor
+	close(comThread);
+}
+
+/*
+ * The program will take in 2 arguments. argv[1] will be the port number (2648)
+ * argv[2] will be the number of iterations the server will perfrom on "read"
+ * from the socket. If the client performs 100 iterations of data transmission,
+ * the server should perform at least 100 reads from the socket to completly
+ * recieve all data sent by the client. So the value should be the same as the
+ * clients iteration argument.
+ */
+int main(int argc, char** argv) {
+	char* port = argv[1]; // 2648 (last four of my student id)
+	int iterations = atoi(argv[2]);
+
+	// Step 1 - Declare an addrinfo structure (defined in netdb.h), initialize it to zero, 
+	// and set its data members to have my port, have an internet family, send streams (not datagrams), and be passive
+	struct addrinfo hints; // how we feed the data from the comment above
+	memset(&hints, 0, sizeof(struct addrinfo));
+	struct addrinfo *result; // a pointer to the first item in the linked list that has our socket info
+	hints.ai_family = AF_UNSPEC; // address family of unspec is the socket address of family ipv4 or 6
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; // I believe this means it is a passive open?
+
+	// Step 2 - Grab the addrinfo guesses linked list and make sure that it could translate
+	// call the getaddrinfo function which returns a linked list of addrinfo for this socket.
+	// im assuming that this linked list is size one because we should be gauranteed the correct answer?
+	// have to dereference result and hints because result is a double pointer and hints is a pointer
+	// the first pointer is used to show the address of the variable and the double pointer points to the address of the pointer
+	// first argument is set to null this time as we have no IP address
+	int canTranslate = getaddrinfo(NULL, port, &hints, &result); // todo: why does the example spec take an int for port but this wants a string?
+
+	// if could successfully translate the address, it will return 0, otherwise, abort
+	if (canTranslate != 0) {
+		cout << "Could not translate address with given hints and port" << endl;
+		exit (EXIT_FAILURE);
+	}
+
+	// Step 3 - try to open the socket with each of the guesses until one works. If none do, abort.
+	// all socket() does is open a socket file on your computer and return the descriptor for it.
+	// if successful, returns the descriptor, if unsuccessful, returns -1
+	// socket() doesnt actually do anything yet, its like building the doorframe without connecting it to a wall yet (cant send or recieve bytes)
+	struct addrinfo* curr = result; // result is the head of the linked list and curr is each record/ node
+
+	int serverSocket = -1;
+
+	while (curr != NULL) {
+		serverSocket = socket(curr->ai_family, curr->ai_socktype, curr->ai_protocol);
+
+		if (serverSocket == -1) {
+			curr = curr->ai_next;
+			continue; // couldnt create a socket so try next addressinfo guess
+		}
+
+		// if it successfully created a socket, we have to configure it
+		// specifically make sure that the listening sockets port can be reused once we create a thread to handle the connection with a new sd
+		// to do this, use setsockopt(). Returns -1 if failes and 0 if succeeds. Arguments:
+		// 1. The sockets file descriptor
+		// 2. The level - the level to manipulate the option
+		// 3. An int for option name - the type of option we want to change
+		// 4. A void pointer optval - the new value we want to set the option to (we are going to assign it the value 1)
+		// 5. Option length - size of previous arguments value
+		// Note: void pointers can hold address of any type and can be typcasted to any type. Cannot be derefrenced
+		int enable = 1; // the value 1 enables the reuse option
+		setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+
+		// Bind the socket descriptor we just created to the PORT we desire
+		// It reserves the port for our current process (whos job is to listen for connections)
+		int successfullyBinded = bind(serverSocket, curr->ai_addr, curr->ai_addrlen);
+
+		// if could bind, returns 0, so we can break and use it
+		if (successfullyBinded == 0) {
+			break;
+		}
+
+		// otherwise, close the socket and try the next guess
+		close(serverSocket);
+	}
+
+	// if serverSocket is still -1, could not create a socket successfully so abort
+	if (serverSocket == -1) {
+		cout << "Could not create a socket with the given port" << endl;
+		exit (EXIT_FAILURE);
+	}
+
+	// free the linked list memory
+	freeaddrinfo(result);
+
+	// Step 4 - Listen on the socket we just created
+	// Tells the OS it is ready to start recieving connections on this port
+	// Needs the serverSocket as a parameter, along with a backlog (number of connections it can handle before it refuses connections)
+	// in this case it is also the number of threads with their own socket descripters handling each users request
+	int listening = listen(serverSocket, 5);
+
+	// if listen returns -1 it means it failed to listen
+	if (listening == -1) {
+		cout << "Could not successfully listen on the given port" << endl;
+		exit (EXIT_FAILURE);
+	}
+
+	// displays that it is listening (just for debugging purposes)
+	cout << "Listening for client connection requests on port " << port << "!" << endl;
+
+	// Step 5 - Keep the server running indefinently, listening for client connection requests and accepting them
+	// this is done via an infinite loop
+	// to accept client connections, we use the accept function which takes in
+	// 1. the server socket descriptor
+	// 2. a pointer for a sockaddr - the client address. Where the accept function will store the location of this
+	// 3. a pointer to the length of the variable pointed to by the previous argument
+	// returns -1 if cannot accept, and a new file descriptor if it does accept
+
+	while (true) {
+		struct sockaddr clientAddress;
+		socklen_t clientAddressLength = sizeof(clientAddress);
+
+		int clientSocketDescriptor = accept(serverSocket, &clientAddress, &clientAddressLength);
+
+		if (clientSocketDescriptor == -1) {
+			cout << "Failed to accept client connection request" << endl;
+			exit(EXIT_FAILURE);
+		}
+
+		// if we were successfully able to accept the connection, we should now create a new thread to handle
+		// the communication with the current client with the new socket descriptor just created
+		pthread_t comThread;
+		struct communicationThreadData* data = new communicationThreadData;
+		data->socketDescriptor = clientSocketDescriptor;
+		data->numIterations = iterations;
+	}
+}
